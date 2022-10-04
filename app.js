b90f922e4d6433623c993326df5d3ae295615a5f
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-10-04 20:11:37 +0300

    cors

--- app.js
+++ app.js
@@ -3,30 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const helmet = require('helmet');
 const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errors');
 const routes = require('./routes/index');
 const DB_ADDRES = require('./utils/dbConstants');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000, NODE_ENV, DB_URL } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [
-      'https://movies.vova.nomoredomains.sbs',
-      'http://movies.vova.nomoredomains.sbs',
-      'https://api.movies.vova.nomoredomains.sbs',
-      'http://api.movies.vova.nomoredomains.sbs',
-    ],
-    credentials: true,
-  }),
-);
+app.use(cors);
 
 app.use(helmet());
 app.use(requestLogger);
---
+++ middlewares/cors.js
@@ -0,0 +1,26 @@
+const allowedCors = [
+  'https://movies.vova.nomoredomains.xyz',
+  'http://movies.vova.nomoredomains.xyz',
+  'https://api.movies.vova.nomoredomains.xyz',
+  'http://api.movies.vova.nomoredomains.xyz',
+];
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+};
+
+module.exports = cors;
